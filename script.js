b3603a5833b8bbfb24dbc855344f13b8446d0ad2
vincaf <vincenzocafassog@gmail.com>
2022-06-21 19:35:01 +0200

    Added style css and event on click <li> item

--- script.js
+++ script.js
@@ -14,8 +14,13 @@ let i = 0;
 while ( i < listaDellaSpesa.length ){
     console.log(listaDellaSpesa[i]);
     const elementoSpesa = document.createElement('li');
+    elementoSpesa.classList.add('p-3', 'list-group-item');
     elementoSpesa.innerText = listaDellaSpesa[i];
     i++;
+
+    elementoSpesa.addEventListener('click', function(){
+    elementoSpesa.classList.add('bg-primary', 'text-white');
+    })
 
     listItem.append(elementoSpesa);
 }
