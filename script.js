3ddec5a569d09c64300bb40baa3871a4b17f0e3e
vincaf <vincenzocafassog@gmail.com>
2022-06-21 18:56:03 +0200

    Added while condition for print on page

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const listaDellaSpesa = [
     'Pasta',
     'Farina',
 ];
+
+const listItem = document.getElementById('list-item');
+
+let i = 0;
+
+while ( i < listaDellaSpesa.length ){
+    console.log(listaDellaSpesa[i]);
+    const elementoSpesa = document.createElement('li');
+    elementoSpesa.innerText = listaDellaSpesa[i];
+    i++;
+
+    listItem.append(elementoSpesa);
+}
